f47bb503c341006a343ca8bf195b36eb7f189a84
M. Nur Arifin <arifinmumtazz@gmail.com>
2016-09-10 14:21:49 +0700

    -edittext nama

--- app/src/main/java/id/sch/smktelkom_mlg/tugas1/xirpl1018/privatelesson/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas1/xirpl1018/privatelesson/MainActivity.java
@@ -2,12 +2,44 @@ package id.sch.smktelkom_mlg.tugas1.xirpl1018.privatelesson;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etNama;
+    Button bOK;
+    TextView tvHasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        bOK = (Button) findViewById(R.id.buttonOK);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+
+        findViewById(R.id.buttonOK).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doClick();
+            }
+        });
+    }
+
+    private void doClick() {
+        String nama = etNama.getText().toString();
+
+        if (nama.isEmpty()) {
+            etNama.setError("Nama belum diisi");
+        } else if (nama.length() <= 3) {
+            etNama.setError("Nama minimal 3 karakter");
+        } else {
+            etNama.setError(null);
+        }
+        tvHasil.setText("Selamat Anda telah mendaftar dengan Nama : " + nama);
     }
 }
